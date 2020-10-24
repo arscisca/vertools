13a603556171d0951f53f49b82ea6e37bb3a79ff
Alessandro Scisca <s276032@studenti.polito.it>
2020-10-24 14:53:06 +0000

    Fix Context structure

--- test/test_context.py
+++ test/test_context.py
@@ -1,24 +1,39 @@
 import vertools
 import vertools.context as vcontext
 
+import pytest
+
+def test_scope():
+    # Test reading
+    scope = vcontext.Scope.from_config(vertools.rootdir/'test/files/example_base.config')
+    assert scope.get('Section 1', 'x') == '1'
+    assert scope.get('Section 1', 'y') == "multi\nline\nvalue"
+    assert scope.get('Section 2', 'z') == '1 ns'
+    # Test error
+    with pytest.raises(vcontext.Scope.NoSectionError):
+        x = scope.get('Nonexisting section', 'x')
+    with pytest.raises(vcontext.Scope.NoParameterError):
+        nonexisting_parameter = scope.get('Section 1', 'nonexisting_parameter')
+
 
 def test_context():
-    # Test reading
-    lower = vcontext.Context.from_config(vertools.rootdir/'test/files/example_base.config')
-    assert lower.get('Section 1', 'x') == '1'
-    assert lower.get('Section 1', 'y') == "multi\nline\nvalue"
-    assert lower.get('Section 2', 'z') == '1 ns'
-
-    # Test overriding
-    upper = vcontext.Context.from_config(vertools.rootdir/'test/files/example_override.config')
-    upper.override(lower)
-    assert upper.get('Section 1', 'x') == 'x was correctly overridden'
-    assert upper.get('Section 1', 'y') == "multi\nline\nvalue"
-    assert upper.get('Section 2', 'z') == '1 ns'
-    assert upper.get('New section', 'new') == 'new parameter now exists'
-
-    # Test relations
-    assert lower.top_level() is upper
-    assert lower.bottom_level() is lower
-    assert upper.top_level() is upper
-    assert upper.bottom_level() is lower
+    context = vcontext.Context()
+    global_scope = vcontext.Scope.from_config(vertools.rootdir/'test/files/example_base.config')
+    local_scope = vcontext.Scope.from_config(vertools.rootdir/'test/files/example_override.config')
+    context.append_local(global_scope)
+    context.append_local(local_scope)
+    # Test relationships
+    assert context.most_global() is global_scope
+    assert context.most_local() is local_scope
+    assert context.most_global().upper is local_scope
+    assert context.most_local().lower is global_scope
+    # Test lookup
+    assert context.get('Section 1', 'x') == 'x was correctly overridden'
+    assert context.get('Section 1', 'y') == "multi\nline\nvalue"
+    assert context.get('Section 2', 'z') == '1 ns'
+    assert context.get('New section', 'new') == 'new parameter now exists'
+    # Test error
+    with pytest.raises(vcontext.Context.LookupError):
+        x = context.get('Nonexisting section', 'x')
+    with pytest.raises(vcontext.Context.LookupError):
+        nonexisting_parameter = context.get('Section 1', 'nonexisting_parameter')
--- vertools/context.py
+++ vertools/context.py
@@ -1,42 +1,95 @@
 import configparser
 
 
-class Context:
-    """Context manager"""
+class Scope:
+    """Scope
+    Attributes:
+        data (configparser.ConfigParser): data
+        upper (Scope): more local scope
+        lower (Scope): more global scope
+    """
     def __init__(self, data=None):
-        self.parent = None
-        self.subcontext = None
         self.data = data
-
-    def top_level(self):
-        if self.parent is not None:
-            return self.parent.top_level()
-        else:
-            return self
-
-    def bottom_level(self):
-        if self.subcontext is not None:
-            return self.subcontext.bottom_level()
-        else:
-            return self
-
-    def override(self, context):
-        self.subcontext = context
-        context.parent = self
-
-    def get(self, section, attribute):
-        if section in self.data:
-            if attribute in self.data[section]:
-                return self.data[section][attribute]
-        if self.subcontext is not None:
-            return self.subcontext.get(section, attribute)
-        raise Context.ContextError(f"Section {section} or attribute {attribute} does not exist")
+        self.upper = None
+        self.lower = None
 
     @classmethod
-    def from_config(cls, file):
+    def from_config(cls, filename):
         config = configparser.ConfigParser()
-        config.read(file)
+        config.read(filename)
         return cls(config)
 
-    class ContextError(LookupError):
+    @classmethod
+    def from_dict(cls, dictionary):
+        config = configparser.ConfigParser()
+        config.read_dict(dictionary)
+        return cls(config)
+
+    def get(self, section, parameter, *args, **kwargs):
+        if self.data.has_section(section):
+            if parameter in self.data[section]:
+                return self.data.get(section, parameter, *args, **kwargs)
+            else:
+                raise Scope.NoParameterError("Parameter not found", parameter)
+        else:
+            raise Scope.NoSectionError("Section not Found")
+
+    def has_section(self, section):
+        return self.data.has_section(section)
+
+    def __contains__(self, section):
+        return section in self.data
+
+    def __getitem__(self, section):
+        return self.data[section]
+
+    class NoSectionError(configparser.NoSectionError):
+        pass
+
+    class NoParameterError(configparser.NoOptionError):
+        pass
+
+
+class Context:
+    def __init__(self):
+        # Generate empty guard scopes
+        self._head = Scope()
+        self._tail = Scope()
+        # Set cross references
+        self._head.lower = self._tail
+        self._tail.upper = self._head
+
+    def place_between(self, scope, first, second):
+        # Insert node
+        first.lower = scope
+        second.upper = scope
+        # Set its connections
+        scope.upper = first
+        scope.lower = second
+
+    def append_local(self, scope):
+        self.place_between(scope, self._head, self.most_local())
+
+    def append_global(self, scope):
+        self.place_between(scope, self.most_global(), self._tail)
+
+    def most_local(self):
+        return self._head.lower
+
+    def most_global(self):
+        return self._tail.upper
+
+    def get(self, section, parameter, *args, **kwargs):
+        return self._get(self.most_local(), section, parameter, *args, **kwargs)
+
+    def _get(self, scope, section, parameter, *args, **kwargs):
+        if section in scope:
+            if parameter in scope[section]:
+                return scope.get(section, parameter, *args, **kwargs)
+        if scope.lower is not self._tail:
+            return self._get(scope.lower, section, parameter)
+        else:
+            raise Context.LookupError(f"Context cannot find parameter {parameter} of section {section} under any scope")
+
+    class LookupError(LookupError):
         pass
